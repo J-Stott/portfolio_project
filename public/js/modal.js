398d1f9aa0bbbd04da32eadfa274f9b5913652e9
J-Stott <tehstott@gmail.com>
2020-07-14 11:46:39 +0100

    Added delete account functionality to profile

---
+++ public/js/modal.js
@@ -0,0 +1,23 @@
+var modal = document.querySelector("#myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector("#delete_account");
+
+var cancelButton = document.querySelector("#close");
+console.log(cancelButton);
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+cancelButton.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
